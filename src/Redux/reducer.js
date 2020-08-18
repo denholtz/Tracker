96e8bdec5997937a9a2745a760aed925b203c461
Mike Denholtz <denholtz@gmail.com>
2020-08-17 19:09:57 -0700

    redux boilerplate

---
+++ src/Redux/reducer.js
@@ -0,0 +1,11 @@
+// import * as catalogActionTypes from '../actionTypes/catalog';
+
+import states from '../../Enums/asyncRequestStates';
+
+export default function(state, action) {
+  switch (action.type) {
+    
+    default:
+      return state;
+  }
+}
