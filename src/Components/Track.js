954a36ebb26aaee5a1fec34886e34bc6202a7bd6
Ada Lerner <ada.r.lerner@gmail.com>
2020-08-21 14:01:49 -0400

    Add redux boilerplate to Track.js

--- src/Components/Track.js
+++ src/Components/Track.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import TrackStep from './TrackStep';
 
+const mapStateToProps = null;
+const mapDispatchToProps = null;
+
 const Track = (props) => {
     const { start, end, innerProps } = props;
     let steps = [];
@@ -10,33 +13,33 @@ const Track = (props) => {
     }
 
     return (
-        <div 
+        <div
             className={innerProps.gridContainerClassName}>
             {steps.map((e, i) => (
                 <React.Fragment key={i}>
-                    <TrackStep 
-                        initiative={e} 
-                        acted={false} 
-                        addPiece={props.addPiece}
-                        movePiece={props.movePiece}
-                        gameState={props.gameState}
-                        onDragEnter={(event) => props.handleDragTargetUpdate(event, e)}
-                        onDragLeave={props.handleDragTargetHide}                           
-                    />
-                    <div >{e % 2 === 0 ? e : ''}</div>
-                    <TrackStep 
-                        initiative={e} 
-                        acted={true} 
+                    <TrackStep
+                        initiative={e}
+                        acted={false}
                         addPiece={props.addPiece}
                         movePiece={props.movePiece}
                         gameState={props.gameState}
                         onDragEnter={(event) => props.handleDragTargetUpdate(event, e)}
                         onDragLeave={props.handleDragTargetHide}
                     />
-                </React.Fragment>                    
+                    <div >{e % 2 === 0 ? e : ''}</div>
+                    <TrackStep
+                        initiative={e}
+                        acted={true}
+                        addPiece={props.addPiece}
+                        movePiece={props.movePiece}
+                        gameState={props.gameState}
+                        onDragEnter={(event) => props.handleDragTargetUpdate(event, e)}
+                        onDragLeave={props.handleDragTargetHide}
+                    />
+                </React.Fragment>
             ))}
         </div>
     )
 }
 
-export default Track;
+export default connect(mapStateToProps, mapDispatchToProps)(Track);
