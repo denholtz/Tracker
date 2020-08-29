4d82fd368829012381ccb5c6f9551f3bd432b92e
Mike Denholtz <denholtz@gmail.com>
2020-08-29 15:40:40 -0700

    added modal control for wp and motes

--- src/Components/ModalNumberSelect.js
+++ src/Components/ModalNumberSelect.js
@@ -1,29 +1,31 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
+import { setNumberSelectModalTarget, updatePiece } from '../Redux/actions';
+
 const mapStateToProps = (state, ownProps) => ({
-    modalNumberSelectTargetPiece: state.modalNumberSelectTargetPiece,
-    modalNumberSelectTargetProperty: state.modalNumberSelectTargetProperty
+    numberSelectModalTarget: state.numberSelectModalTarget
 })
 
 const mapDispatchToProps = {
-
+    setNumberSelectModalTarget, 
+    updatePiece
 }
 
-const styles = (theme) => ({
-
-});
-
 const ModalNumberSelect = (props) => {
-    const { modalNumberSelectTargetPiece, modalNumberSelectTargetProperty } = props;
+    const { numberSelectModalTarget, setNumberSelectModalTarget, updatePiece } = props;
+
+    if(!numberSelectModalTarget || !numberSelectModalTarget.pieceID) return '';
+
+    const { pieceID, attribute } = numberSelectModalTarget;
 
     return (
         <React.Fragment>
-            <div className='ui-blocker' style={modalNumberSelectTargetPiece ? {display: 'none'} : {}}>
+            <div className={`ui-blocker ${numberSelectModalTarget ? '' : 'display-none'}`} onClick={() => setNumberSelectModalTarget(null)}>
                 <div id='modal-number-select'>
                     {new Array(60).fill(0).map((e, i) => (
-                        <div className='modal-number-select-number' key={i}>
-                            {i}
+                        <div className='modal-number-select-number' key={i} onClick={() => updatePiece(pieceID, attribute, i)}>
+                            <span style={{display:'block'}}>{i}</span>
                         </div>
                     ))}
                 </div>
